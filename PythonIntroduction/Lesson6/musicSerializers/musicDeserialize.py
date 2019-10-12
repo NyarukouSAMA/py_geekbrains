1decfd15835a4521fd68f9332d22443d45583661
NyarukouSAMA <afro.detray@gmail.com>
2019-10-12 12:13:08 +0300

    Lesson6

---
+++ PythonIntroduction/Lesson6/musicSerializers/musicDeserialize.py
@@ -0,0 +1,20 @@
+import os.path
+import pickle
+import json
+
+
+def getFromPickle(filename):
+    filename += ".pickle"
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+
+def getFromJson(filename):
+    filename += ".json"
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    print(getFromPickle('group'))
+    print(getFromJson('group'))
---
+++ PythonIntroduction/Lesson6/musicSerializers/musicSerialize.py
@@ -0,0 +1,44 @@
+import json
+import pickle
+
+
+def getJson(item):
+    return json.dumps(item)
+
+
+def getPickle(item):
+    return pickle.dumps(item)
+
+
+def createJsonFile(item, filename):
+    if type(filename) != str:
+        return (False, 'Имя файла должно быть строкой')
+    filename += ".json"
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(item, f)
+    return (True, 'Файл сохранен в формате json')
+
+
+def createPickleFile(item, filename):
+    if type(filename) != str:
+        return (False, 'Имя файла должно быть строкой')
+    filename += ".pickle"
+    with open(filename, 'wb') as f:
+        pickle.dump(item, f)
+    return (True, 'Файл сохранен в формате pickle')
+
+
+if __name__ == "__main__":
+    myFavouriteGroup = \
+        {'name': 'Г.М.О.',
+         'tracks': ['Последний месяц осени', 'Шапито'],
+         'Albums': [{'name': 'Делать панк-рок', 'year': 2016},
+                    {'name': 'Шапито', 'year': 2014}]}
+    jsonString = getJson(myFavouriteGroup)
+    print(type(jsonString))
+    print(jsonString)
+    pickledObject = getPickle(myFavouriteGroup)
+    print(type(pickledObject))
+    print(pickledObject)
+    createJsonFile(myFavouriteGroup, 'group')
+    createPickleFile(myFavouriteGroup, 'group')
